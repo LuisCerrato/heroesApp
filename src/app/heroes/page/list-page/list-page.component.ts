8a01dddd8c745323df7665c42eef179e01dd5cd8
LuisCerrato <jlcerrato@bancatlan.hn>
2024-03-25 15:04:13 -0600

    feat: adding services first view

--- src/app/heroes/page/list-page/list-page.component.ts
+++ src/app/heroes/page/list-page/list-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesServices } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-list-page',
-  templateUrl: './list-page.component.html',
-  styles: ``
+  templateUrl: './list-page.component.html'
 })
-export class ListPageComponent {
+export class ListPageComponent  implements OnInit {
+
+  public heroes : Hero[] = [];
+
+  constructor(
+
+    private HeroesServices : HeroesServices
+  ){}
+
+  ngOnInit(): void {
+
+    this.HeroesServices.getHeroes()
+    .subscribe( heroes => this.heroes = heroes )
+  }
 
 }
